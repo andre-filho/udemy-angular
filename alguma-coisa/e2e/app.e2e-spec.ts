06890dc519d2147610432d7c4089b0c99e4467d9
Andre Filho <dedaoskater@hotmail.com>
2018-01-26 10:35:08 -0200

    [UPDATE] lecture 16

--- alguma-coisa/e2e/app.e2e-spec.ts
+++ alguma-coisa/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('my-first-app App', () => {
+describe('alguma-coisa App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- shit/e2e/app.e2e-spec.ts
+++ shit/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('my-first-app App', () => {
+describe('shit App', () => {
   let page: AppPage;
 
   beforeEach(() => {
