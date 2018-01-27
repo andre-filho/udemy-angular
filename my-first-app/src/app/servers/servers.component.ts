1a717a99bb0de93cbcf163fe6f900fbd422dc28a
Andre Filho <dedaoskater@hotmail.com>
2018-01-27 10:41:34 -0200

    [UPDATE] lecture 22

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // other forms of selecting a component
+  selector: 'app-servers',   // normal stuff
+  // selector: '[app-servers]', property selecting
+  // selector: '.app-servers',  class selecting
+  // be aware that u cannot select by id
   template: `
     <app-server></app-server>
     <app-server></app-server>`,
