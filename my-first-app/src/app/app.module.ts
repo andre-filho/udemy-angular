5bd67ebe1933f8b68fb1a36814785f9dc14b205c
Andre Filho <dedaoskater@hotmail.com>
2018-01-29 18:44:49 -0200

    [ADD] component for assignment 2

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { Assignment2Component } from './assignment2/assignment2.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     ServerComponent,
     ServersComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    Assignment2Component
   ],
   imports: [
     BrowserModule,
