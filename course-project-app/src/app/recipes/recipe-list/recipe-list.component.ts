2c1886c5a5c8e70409038369ee3078510b384262
Andre Filho <andre.filho001@outlook.com>
2018-02-01 15:25:33 -0200

    [UPDATE] lecture 49

--- course-project-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './../recipe.module';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'simple desc', 'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg'),
+    new Recipe('A Test Recipe', 'simple desc', 'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg')
+  ];
 
   constructor() { }
 
