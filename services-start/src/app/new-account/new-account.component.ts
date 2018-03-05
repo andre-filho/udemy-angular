6bb296a1c3fca7553b03955ad217484731f3d78c
Andre Filho <andre.filho001@outlook.com>
2018-03-05 20:15:14 -0300

    [UPDATE] lecture 102
    Injecting services into services to serve the servicing proporse of serving.

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -10,16 +10,16 @@ import { AccountsService } from '../accounts.service';
   styleUrls: ['./new-account.component.css'],
   // tells angular to provide the thingy that we need for the service
   // AccountsService was removed because of the hierarchical thingy 
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
   // this will inject the dependency
-  constructor(private loggingService:  LoggingService,
+  constructor(private loggingService:  LoggingService,  //  unnecessary
               private accountsService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
 
     // console.log('A server status changed, new status: ' + accountStatus);
 
